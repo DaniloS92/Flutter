59cf5094fa7b52eb3e6dae5e0025e19c53440ad0
Danilo Sanchez <danilo_das92@hotmail.com>
2020-09-24 22:59:21 -0500

    [UPD] Clase clase 122

--- peliculas/lib/src/search/search_delegate.dart
+++ peliculas/lib/src/search/search_delegate.dart
@@ -4,24 +4,39 @@ class DataSearch extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones de nuestro Appbar
-    throw UnimplementedError();
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     // Icono a la izquierda del appbar
-    throw UnimplementedError();
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados que vamos a mostrar
-    throw UnimplementedError();
+    return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Sugerencias que aparecen cuando la persona escribe
-    throw UnimplementedError();
+    return Container();
   }
 }
