76e267dba4b718d7a49088a2ef59d3330275e5fd
Danilo Sanchez <danilo_das92@hotmail.com>
2020-10-02 16:04:05 -0500

    [UPD] Clase 137

--- disenos/lib/src/pages/scroll_page.dart
+++ disenos/lib/src/pages/scroll_page.dart
@@ -18,13 +18,70 @@ class ScrollPage extends StatelessWidget {
 
   Widget _pagina1() {
     return Container(
-      child: Center(child: Text("pagina 1")),
-    );
+        child: Stack(
+      children: [
+        _colorFondo(),
+        _imagenFondo(),
+        _textoFondo(),
+      ],
+    ));
   }
 
   Widget _pagina2() {
     return Container(
-      child: Center(child: Text("pagina 2")),
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+      child: Center(
+        child: RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+            child: Text(
+              "Bienvenidos",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+
+  _colorFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage("assets/img/scroll-1.png"),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  _textoFondo() {
+    final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 20.0),
+          Text("11°", style: estiloTexto),
+          Text("Miércoles", style: estiloTexto),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white)
+        ],
+      ),
     );
   }
 }
