4eb1385c0ac4f6228da387bd5f0d6bfb3cf608ef
Danilo Sanchez <danilo_das92@hotmail.com>
2020-10-04 22:29:22 -0500

    [UPD] Clase 139

--- disenos/lib/src/pages/botones_page.dart
+++ disenos/lib/src/pages/botones_page.dart
@@ -17,9 +17,10 @@ class BotonesPage extends StatelessWidget {
                 _titulos(),
               ],
             ),
-          )
+          ),
         ],
       ),
+      bottomNavigationBar: _bottomNavigationBar(context),
     );
   }
 
@@ -92,5 +93,40 @@ class BotonesPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _bottomNavigationBar(BuildContext context) {
+    return Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
+          primaryColor: Colors.pinkAccent,
+          textTheme: Theme.of(context).textTheme.copyWith(
+              caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0))),
+        ),
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.calendar_today,
+                size: 30.0,
+              ),
+              title: Container(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.bubble_chart,
+                size: 30.0,
+              ),
+              title: Container(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.supervised_user_circle,
+                size: 30.0,
+              ),
+              title: Container(),
+            )
+          ],
+        ));
   }
 }
