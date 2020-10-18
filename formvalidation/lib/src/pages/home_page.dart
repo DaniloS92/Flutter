a265e8785bde3a231ca68c4b180b0ddcf05cdbfc
Danilo Sanchez <danilo_das92@hotmail.com>
2020-10-17 21:58:10 -0500

    [UPD] Aplicacion terminada

--- formvalidation/lib/src/pages/home_page.dart
+++ formvalidation/lib/src/pages/home_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Email: ${bloc.email}'),
+            Divider(),
+            Text('Password: ${bloc.pass}'),
+          ],
+        ),
       ),
     );
   }
--- formvalidation/lib/src/pages/login_page.dart
+++ formvalidation/lib/src/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/provider.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key key}) : super(key: key);
@@ -64,6 +65,7 @@ class LoginPage extends StatelessWidget {
   }
 
   _loginForm(BuildContext context) {
+    final bloc = Provider.of(context);
     final size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
@@ -92,14 +94,15 @@ class LoginPage extends StatelessWidget {
                   style: TextStyle(fontSize: 20.0),
                 ),
                 SizedBox(height: 60.0),
-                _crearEmail(),
+                _crearEmail(bloc),
                 SizedBox(height: 30.0),
-                _crearPass(),
+                _crearPass(bloc),
                 SizedBox(height: 30.0),
-                _crearBoton()
+                _crearBoton(bloc)
               ],
             ),
           ),
+          SizedBox(height: 10.0),
           Text("Olvido la contraseña"),
           SizedBox(
             height: 100.0,
@@ -109,50 +112,79 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  _crearEmail() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: InputDecoration(
-          icon: Icon(
-            Icons.alternate_email,
-            color: Colors.deepPurple,
-          ),
-          hintText: 'ejemplo@correo.com',
-          labelText: 'Correo electronico',
-        ),
-      ),
-    );
+  _crearEmail(LoginBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.emailStream,
+        builder: (context, snapshot) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.alternate_email,
+                  color: Colors.deepPurple,
+                ),
+                hintText: 'ejemplo@correo.com',
+                labelText: 'Correo electronico',
+                counterText: snapshot.data,
+                errorText: snapshot.error,
+              ),
+              onChanged: bloc.changeEmail,
+            ),
+          );
+        });
   }
 
-  _crearPass() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: TextField(
-        obscureText: true,
-        decoration: InputDecoration(
-          icon: Icon(
-            Icons.lock_outline,
-            color: Colors.deepPurple,
-          ),
-          labelText: 'Contraseña',
-        ),
-      ),
-    );
+  _crearPass(LoginBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.passStream,
+        builder: (context, snapshot) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.lock_outline,
+                  color: Colors.deepPurple,
+                ),
+                labelText: 'Contraseña',
+                counterText: snapshot.data,
+                errorText: snapshot.error,
+              ),
+              onChanged: bloc.changePassword,
+            ),
+          );
+        });
   }
 
-  _crearBoton() {
-    return RaisedButton(
-      onPressed: () {},
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-        child: Text("Ingresar"),
-      ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-      elevation: 0.0,
-      color: Colors.deepPurple,
-      textColor: Colors.white,
-    );
+  _crearBoton(LoginBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.formValidateStream,
+        builder: (context, snapshot) {
+          return RaisedButton(
+            onPressed: (snapshot.hasData) ? () => _login(context, bloc) : null,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+              child: Text("Ingresar"),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            elevation: 0.0,
+            color: Colors.deepPurple,
+            textColor: Colors.white,
+          );
+        });
+  }
+
+  _login(BuildContext context, LoginBloc bloc) {
+    print("===============");
+    print("Correo: ${bloc.email}");
+    print("Password: ${bloc.pass} ");
+    print("===============");
+
+    Navigator.pushReplacementNamed(context, 'home');
   }
 }
