a7e2d82d5c3f42cc07d584f28242bcea5515362b
Danilo Sanchez <danilo_das92@hotmail.com>
2020-10-21 21:46:54 -0500

    [UPD] Clase 220

--- formvalidation/lib/src/pages/home_page.dart
+++ formvalidation/lib/src/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/bloc/provider.dart';
+import 'package:formvalidation/src/models/producto.dart';
+import 'package:formvalidation/src/providers/productos_provider.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  final productosProvider = new ProductosProvider();
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +14,48 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Container(),
+      body: _crearListado(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         backgroundColor: Colors.deepPurple,
         onPressed: () {
           Navigator.pushNamed(context, 'producto');
         },
+      ),
+    );
+  }
+
+  Widget _crearListado() {
+    return FutureBuilder(
+      future: productosProvider.cargarProductos(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
+        if (snapshot.hasData) {
+          final productos = snapshot.data;
+          return ListView.builder(
+            itemCount: productos.length,
+            itemBuilder: (context, index) {
+              return _crearLista(productos[index], index, context);
+            },
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  Dismissible _crearLista(ProductoModel prod, int index, BuildContext context) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(color: Colors.red),
+      onDismissed: (direction) {
+        //Todo: Borrar la wea de producto
+      },
+      child: ListTile(
+        title: Text('${prod.titulo} - ${prod.valor}'),
+        subtitle: Text(prod.id),
+        onTap: () => Navigator.pushNamed(context, 'producto'),
       ),
     );
   }
--- formvalidation/lib/src/providers/productos_provider.dart
+++ formvalidation/lib/src/providers/productos_provider.dart
@@ -14,4 +14,20 @@ class ProductosProvider {
     print(decodedData);
     return true;
   }
+
+  Future<List<ProductoModel>> cargarProductos() async {
+    final url = '$_url/productos.json';
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(resp.body);
+    final List<ProductoModel> productos = new List();
+
+    if (decodedData == null) return [];
+    decodedData.forEach((id, product) {
+      final prodTemp = ProductoModel.fromJson(product);
+      prodTemp.id = id;
+      productos.add(prodTemp);
+    });
+    return productos;
+  }
 }
